1cf741835a37d177bf0df96a33f97c07dc1fc72f
Josh Bechard <josh@oshbec.dev>
2020-10-25 21:08:07 -0500

    Set up development database

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :rumbl, Rumbl.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "rumbl",
+  password: "b1aac842-f913-4339-8c08-5ab9e6f8e89c",
   database: "rumbl_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
