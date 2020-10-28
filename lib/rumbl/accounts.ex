cc4862f4a7696a145acada2bdc0e0f5abdf1a204
Josh Bechard <josh@oshbec.dev>
2020-10-27 19:03:15 -0500

    Add user persistence

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -2,24 +2,36 @@ defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts context.
   """
-
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
   def list_users do
-    [
-      %User{id: "1", name: "Josh", username: "oshbec"},
-      %User{id: "2", name: "Chris", username: "dilcue"},
-      %User{id: "3", name: "Kevin", username: "nake"}
-    ]
+    Repo.all(User)
   end
 
   def get_user(id) do
-    Enum.find(list_users(), fn user -> user.id == id end)
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn user ->
-      Enum.all?(params, fn {key, val} -> Map.get(user, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def get_user_by!(params) do
+    Repo.get_by!(User, params)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
   end
 end
