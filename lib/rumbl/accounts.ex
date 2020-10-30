4f0066612638b869de607dc94fdbf1824caeac90
Josh Bechard <josh@oshbec.dev>
2020-10-29 20:56:13 -0500

    Handle sessions

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -44,4 +44,20 @@ defmodule Rumbl.Accounts do
     |> User.registration_changeset(attrs)
     |> Repo.insert()
   end
+
+  def authenticate_by_username_and_password(username, given_password) do
+    user = get_user_by(username: username)
+
+    cond do
+      user && Pbkdf2.verify_pass(given_password, user.password_hash) ->
+        {:ok, user}
+
+      user ->
+        {:error, :unauthorized}
+
+      true ->
+        Pbkdf2.no_user_verify()
+        {:error, :not_found}
+    end
+  end
 end
