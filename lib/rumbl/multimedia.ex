609c038e83c2b50fdf5c8abe2f3b47dd085f0f90
Josh Bechard <josh@oshbec.dev>
2020-10-31 22:51:01 -0500

    Add create / list alphabetical categories

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -7,7 +7,17 @@ defmodule Rumbl.Multimedia do
   alias Rumbl.Repo
 
   alias Rumbl.Accounts
-  alias Rumbl.Multimedia.Video
+  alias Rumbl.Multimedia.{Video, Category}
+
+  def create_category!(name) do
+    Repo.insert!(%Category{name: name}, on_conflict: :nothing)
+  end
+
+  def list_alphabetical_categories do
+    Category
+    |> Category.alphabetical()
+    |> Repo.all()
+  end
 
   @doc """
   Returns the list of videos.
